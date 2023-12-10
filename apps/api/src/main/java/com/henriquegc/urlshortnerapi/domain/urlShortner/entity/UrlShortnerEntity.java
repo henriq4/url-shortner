23b9111b4284fede7c45dd735427cf78f5f1d465
Henrique GC <henriq4NET@gmail.com>
2023-12-10 13:40:50 -0300

    chore(api): using lambook to generate getters and setters

--- apps/api/src/main/java/com/henriquegc/urlshortnerapi/domain/urlShortner/entity/UrlShortnerEntity.java
+++ apps/api/src/main/java/com/henriquegc/urlshortnerapi/domain/urlShortner/entity/UrlShortnerEntity.java
@@ -1,6 +1,8 @@
 package com.henriquegc.urlshortnerapi.domain.urlShortner.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,29 +12,17 @@ public class UrlShortnerEntity {
     @Id
     private String id;
 
+    @Getter
+    @Setter
     @Indexed(unique = true)
     private String code;
 
+    @Getter
+    @Setter
     private String url;
 
     public UrlShortnerEntity(String code, String url) {
         this.code = code;
         this.url = url;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getUrl() {
-        return url;
     }
 }
