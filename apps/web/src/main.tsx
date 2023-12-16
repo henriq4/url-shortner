5e0d509b7846b26f339055d388fb67464b5357f2
Henrique GC <henriq4NET@gmail.com>
2023-12-15 22:29:13 -0300

    feat(web): react router dom to handle with routers

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Home from "./pages/Home.tsx";
-
 import "./styles/globals.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
